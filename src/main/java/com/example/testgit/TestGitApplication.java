ba569588493cbdd56d3ef63937d8112d8c6f7a75
Rom Netanel Vinnitski <romnetanel11@gmail.com>
2023-01-15 17:59:30 +0200

    added BABI

--- src/main/java/com/example/testgit/TestGitApplication.java
+++ src/main/java/com/example/testgit/TestGitApplication.java
@@ -14,6 +14,7 @@ public class TestGitApplication {
         System.out.println("Hello World!");
         System.out.println("My name is Rom Netanel Vinnitski.");
         System.out.println("my partner is Ariel Dobkin");
+        System.out.println("and i have another partner whom name is Nehorai.")
     }
 
 }
